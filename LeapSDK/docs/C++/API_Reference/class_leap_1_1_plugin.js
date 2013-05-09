a2e3f92ffeb7c0ce2c7aaa63da095877e485521d
Will Whitney <wwhitney@mit.edu>
2013-05-08 20:58:28 -0400

    add Leap SDK

---
+++ LeapSDK/docs/C++/API_Reference/class_leap_1_1_plugin.js
@@ -0,0 +1,12 @@
+var class_leap_1_1_plugin =
+[
+    [ "Plugin", "class_leap_1_1_plugin.html#ae5cdc278dd0694fa7a2f03206ac4de27", null ],
+    [ "~Plugin", "class_leap_1_1_plugin.html#a4476a76936074a183d6b13e2e50560d9", null ],
+    [ "config", "class_leap_1_1_plugin.html#a3049382bec723ab41d7f3738237b4595", null ],
+    [ "onConnect", "class_leap_1_1_plugin.html#aadf6f0d93a959c9a50d23adabfd5e4e6", null ],
+    [ "onDisconnect", "class_leap_1_1_plugin.html#ae5a893fa3408b3c19edc93d01128ca27", null ],
+    [ "onFocusGained", "class_leap_1_1_plugin.html#acf48988df928c85dcbf952edc4de0552", null ],
+    [ "onFocusLost", "class_leap_1_1_plugin.html#ad51018501d23484b9fdae5f608af04ee", null ],
+    [ "onFrame", "class_leap_1_1_plugin.html#a476d38b410f44d49ccd1c833fc50a3f9", null ],
+    [ "onInit", "class_leap_1_1_plugin.html#aeba9f64be19415254ce17d52118cf392", null ]
+];
---
+++ LeapSDK/docs/Java/API_Reference/navtreeindex2.js
@@ -0,0 +1,7 @@
+var NAVTREEINDEX2 =
+{
+"functions_vars.html":[0,3,2],
+"hierarchy.html":[0,2],
+"index.html":[],
+"pages.html":[]
+};

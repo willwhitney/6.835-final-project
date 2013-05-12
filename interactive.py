ab6c6dfc38f76d2a8775fba320517cc41f07c262
Cristina <clozano@mit.edu>
2013-05-12 13:44:17 -0400

    added graphics

--- interactive.py
+++ interactive.py
@@ -1,3 +1,6 @@
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL import *
 import Leap, sys
 
 firstHistoryFrame = 30
@@ -97,16 +100,61 @@ class GestureController(Leap.Listener):
 
 
 
+ 
+# This function is called whenever a "Normal" key press is received.
+def keyboardFunc( key, x, y ):
+    # exit if and ENTER or ESC is pressed
+    if key == '\r' or key == '\x1b':
+        exit()
+    elif key == 'r':
+        glRotatef(30.0, 1, 0, 0)
+        glutPostRedisplay()
 
 
+def drawScene():
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glPushMatrix()
+    
+    # Position the camera at [0,0,20], looking at [0,0,0],
+    # with [0,1,0] as the up direction.
+    
+    color = [0.5, 0.5, 0.9, 1.0]
+    glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
+    glutWireTeapot(1)
+    glPopMatrix()
+    glutSwapBuffers()
+    return
 
 
-
-
-
+def initRendering():
+    glEnable(GL_DEPTH_TEST);   # Depth testing must be turned on
+    glEnable(GL_LIGHTING);     # Enable lighting calculations
+    glEnable(GL_LIGHT0);       # Turn on light #0.
+    
+    glClearColor(0.,0.,0.,1.)
+    glShadeModel(GL_SMOOTH)
+    glEnable(GL_CULL_FACE)
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_LIGHTING)
+    lightZeroPosition = [10.,4.,10.,1.]
+    lightZeroColor = [0.8,1.0,0.8,1.0] #green tinged
+    glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
+    glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
+    glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
+    glEnable(GL_LIGHT0)
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(40.,1.,1.,40.)
+    glMatrixMode(GL_MODELVIEW)
+    gluLookAt(0,0,20,
+              0,0,0,
+              0,1,0)
+    glPushMatrix()
 
 
 def main():
+    print "Press Enter to quit..."
+  
     # Create a sample listener and controller
     listener = GestureController()
     controller = Leap.Controller()
@@ -114,12 +162,24 @@ def main():
     # Have the sample listener receive events from the controller
     controller.add_listener(listener)
 
-    # Keep this process running until Enter is pressed
-    print "Press Enter to quit..."
-    sys.stdin.readline()
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(800,800)
+    glutInitWindowPosition(0,0)
+    glutCreateWindow("Leap Visualizer")
 
-    # Remove the sample listener when done
-    controller.remove_listener(listener)
+    # Initialize OpenGL parameters.
+    initRendering()
+    
+    # Set up callback functions for key presses
+    glutKeyboardFunc(keyboardFunc) # Handles "normal" ascii symbols
+    
+    #Call this whenever window needs redrawing
+    glutDisplayFunc(drawScene)
+    
+    glutMainLoop()
+    return
+    
 
 
 if __name__ == "__main__":

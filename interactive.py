ea9b8dcc3f6f042ff3cbb5d24b24c464189b6c71
Will Whitney <wwhitney@mit.edu>
2013-05-12 23:51:46 -0400

    OpenGL appears to work now. Is still sort of bitchy, but all directions correct.

--- interactive.py
+++ interactive.py
@@ -1,7 +1,7 @@
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
-import Leap, sys
+import Leap, sys, Queue
 
 firstHistoryFrame = 30
 lastHistoryFrame = 40
@@ -10,17 +10,29 @@ numPastFrames = lastHistoryFrame - firstHistoryFrame
 averageWindow = 5
 
 fingerDecreaseThreshold = 2
-fingerIncreaseThreshold = 1
+fingerIncreaseThreshold = 0
 radiusDecreaseThreshold = 5
 
 palmVelocityThreshold = 700
 
-center = [0,0,0]
+queue = Queue.Queue()
+
+center = [0, 0, 0]
+angle = [0, 0, 0]
+translation = [0, 0, 0]
+rotation = [0, 0, 0]
 
 class GestureController(Leap.Listener):
     def on_init(self, controller):
         self.state = 'open'
+        self.translated = False
         print "Initialized"
+        glutPostRedisplay()
+        # glTranslate(1.2, 3.5, 8.0)
+        # glutPostRedisplay()
+
+    def setQueue(self, queue):
+        self.queue = queue
 
     def on_connect(self, controller):
         print "Connected"
@@ -29,13 +41,16 @@ class GestureController(Leap.Listener):
         # Note: not dispatched when running in a debugger.
         print "Disconnected"
 
-    def on_exit(self, controller):
-        print "Exited"
-
     def average_hands(self, controller, start, stop):
         hand = {
             'sphere_radius': 0,
-            'numFingers': 0
+            'numFingers': 0,
+            'x': 0,
+            'y': 0,
+            'z': 0,
+            'pitch': 0,
+            'roll': 0,
+            'yaw': 0
         }
         for i in xrange(start, stop, 1):
             frame = controller.frame(i)
@@ -44,10 +59,21 @@ class GestureController(Leap.Listener):
             thisHand = frame.hands[0]
             hand['sphere_radius'] += thisHand.sphere_radius
             hand['numFingers'] += len(thisHand.fingers)
-
+            hand['x'] += thisHand.palm_position.x 
+            hand['y'] += thisHand.palm_position.y
+            hand['z'] += thisHand.palm_position.z
+            hand['pitch'] += thisHand.direction.pitch * Leap.RAD_TO_DEG
+            hand['roll'] += thisHand.palm_normal.roll * Leap.RAD_TO_DEG
+            hand['yaw'] += thisHand.direction.yaw * Leap.RAD_TO_DEG
         hand = {
             'sphere_radius': hand['sphere_radius'] / (stop - start),
-            'numFingers': float(hand['numFingers']) / (stop - start)
+            'numFingers': float(hand['numFingers']) / (stop - start),
+            'x': hand['x'] / (stop - start),
+            'y': hand['y'] / (stop - start),
+            'z': hand['z'] / (stop - start),
+            'pitch': hand['pitch'] / (stop - start),
+            'roll': hand['roll'] / (stop - start),
+            'yaw': hand['yaw'] / (stop - start)
         }
         return hand
 
@@ -79,6 +105,7 @@ class GestureController(Leap.Listener):
                             self.state = 'closed'
                             return
 
+
                     # print frame.hands[0]
 
                 else:
@@ -92,47 +119,88 @@ class GestureController(Leap.Listener):
                             self.state = 'open'
                             return
 
-                    hand = frame.hands[0]
-                    print hand.palm_position
-                    print (hand.direction.pitch * Leap.RAD_TO_DEG,
-                            hand.palm_normal.roll * Leap.RAD_TO_DEG,
-                            hand.direction.yaw * Leap.RAD_TO_DEG)
+                        lastHand = self.average_hands(controller, averageWindow + 1, 2 * averageWindow)
+                        translation = (
+                                hand['x'] - lastHand['x'], 
+                                hand['y'] - lastHand['y'], 
+                                hand['z'] - lastHand['z']
+                        )
+                        rotation = (
+                                hand['pitch'] - lastHand['pitch'], 
+                                hand['roll'] - lastHand['roll'], 
+                                hand['yaw'] - lastHand['yaw']
+                        )
+                        # print rotation
+                        queue.put(translation + rotation)
+
 
         else:
             if not self.recent_hands(controller, 10):
                 self.state = 'open'
 
+def updatePosition():
+    global center
+    global angle
+    while not queue.empty():
+        movement = list(queue.get_nowait())
+        translation = [movement[i] / 200 for i in range(3)]
+        rotation = [movement[i] / 2 for i in range(3, 6)]
 
+        center = [center[i] + translation[i] for i in range(3)]
+        angle = [angle[i] + rotation[i] for i in range(3)]
 
+    # glLoadIdentity()
+    
+    # glRotatef(angle[0], 1, 0, 0)
+    # glRotatef(angle[1], 0, 1, 0)
+    # glRotatef(angle[2], 0, 0, 1)
+    # glTranslate(*center)
 
+    glutPostRedisplay()
 
  
 # This function is called whenever a "Normal" key press is received.
 def keyboardFunc( key, x, y ):
-    # exit if and ENTER or ESC is pressed
+    # exit if ENTER or ESC is pressed
     if key == '\r' or key == '\x1b':
         exit()
     elif key == 'r':
-        glTranslate(-center[0], -center[1], -center[2])
-        glRotatef(30.0, 1, 0, 0)
-        glTranslate(center[0], center[1], center[2])
+        angle[0] += 20
+        # glTranslate(-center[0], -center[1], -center[2])
+        # glRotatef(30.0, 1, 0, 0)
+        # glTranslate(center[0], center[1], center[2])
         glutPostRedisplay()
     elif key == 't':
-        glTranslate(1,0,0)
         center[0] += 1
-        glutPostRedisplay()
+        # glTranslate(1,0,0)
+        # center[0] += 1
+        # glutPostRedisplay()
 
 def drawScene():
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
     glPushMatrix()
+
+    # reverseCenter = [-1 * center[i] for i in range(3)]
+    glLoadIdentity();
+
+    print center, angle
+    glTranslate(0, 0, -10)
+    # glTranslate(*reverseCenter)
+    glTranslate(*center)
+    glRotatef(angle[0], 1, 0, 0)
+    glRotatef(angle[1], 0, 0, 1)
+    glRotatef(-1 * angle[2], 0, 1, 0)
+    
     
     # Position the camera at [0,0,20], looking at [0,0,0],
-    # with [0,1,0] as the up direction.
-    
+    # with [0,1,0] as the up direction.    
+
     color = [0.5, 0.5, 0.9, 1.0]
     glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
     glutWireTeapot(1)
+
     glPopMatrix()
+
     glutSwapBuffers()
     return
 
@@ -155,24 +223,14 @@ def initRendering():
     glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
     glEnable(GL_LIGHT0)
     glMatrixMode(GL_PROJECTION)
-    gluPerspective(40.,1.,1.,40.)
+    gluPerspective(60.,1.,1.,60.)
     glMatrixMode(GL_MODELVIEW)
-    gluLookAt(0,0,20,
-              0,0,0,
+    gluLookAt(0,0,0,
+              0,0,-10,
               0,1,0)
     glPushMatrix()
 
-
-def main():
-    print "Press Enter to quit..."
-  
-    # Create a sample listener and controller
-    listener = GestureController()
-    controller = Leap.Controller()
-
-    # Have the sample listener receive events from the controller
-    controller.add_listener(listener)
-
+def openGLSetup():
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
     glutInitWindowSize(800,800)
@@ -184,11 +242,26 @@ def main():
     
     # Set up callback functions for key presses
     glutKeyboardFunc(keyboardFunc) # Handles "normal" ascii symbols
+    glutIdleFunc(updatePosition)
     
     #Call this whenever window needs redrawing
     glutDisplayFunc(drawScene)
-    
     glutMainLoop()
+
+def main():
+    print "Press Enter to quit..."
+
+    # Create a sample listener and controller
+    listener = GestureController()
+    controller = Leap.Controller()
+
+    listener.setQueue(queue)
+
+    # Have the sample listener receive events from the controller
+    controller.add_listener(listener)
+
+    openGLSetup()   
+    
     return
     
 
